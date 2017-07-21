0bb76a241132bf743313a951306e28b31e164019
Moritz Ulrich <moritz@tarn-vedra.de>
2017-07-21 14:40:56 +0200

    Implement EAVT index for in-memory. Open: Sqlite.

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,8 @@
 extern crate rand;
 extern crate rusqlite;
 
+use std::borrow::Borrow;
+
 #[derive(Debug, PartialEq, Eq, Clone, Copy, PartialOrd, Ord)]
 pub struct EntityId(i64);
 
@@ -46,17 +48,48 @@ pub struct Entity<'a, D: Db + 'a> {
 use std::borrow::Cow;
 pub type Datoms<'a> = Cow<'a, [Datom]>;
 
+#[derive(Debug, Clone, Copy)]
+pub enum Index {
+  Eavt,
+  // Avet,
+  // Aevt,
+  // Vaet,
+}
+
+#[derive(Debug)]
+pub struct Components(Option<EntityId>,
+                      Option<Attribute>,
+                      Option<Value>,
+                      Option<TxId>);
+
+impl Components {
+  pub fn empty() -> Self {
+    Components(None, None, None, None)
+  }
+
+  pub fn matches(&self, datom: &Datom) -> bool {
+    let &Components(e, a, ref v, t) = self;
+
+    let e = e.is_none() || e.unwrap() == datom.entity;
+    let a = a.is_none() || a.unwrap() == datom.attribute;
+    let v = v.is_none() || v.as_ref().unwrap() == &datom.value;
+    let t = t.is_none() || t.unwrap() == datom.tx;
+
+    return e && a && v && t;;
+  }
+}
+
 pub trait Db: Sized {
   fn transact<T: Into<Fact>>(&mut self, tx: &[T]) -> TxId;
-  fn all_datoms<'a>(&'a self) -> Datoms; // TODO: impl Iter?
-    
+  fn datoms<'a, C: Borrow<Components>>(&'a self, index: Index, components: C) -> Datoms;
+
   fn entity<'a>(&'a self, entity: EntityId) -> Entity<'a, Self> {
-    let datoms = self.all_datoms();
+    let datoms = self.datoms(Index::Eavt, Components::empty());
     let mut attrs: BTreeMap<Attribute, (TxId, &Datom)> = Default::default();
-    
+
     for d in datoms.into_iter().filter(|d| d.entity == entity) {
       use std::collections::btree_map::Entry;
-      
+
       let entry = (d.tx, d);
       match attrs.entry(d.attribute) {
         Entry::Vacant(e) => { e.insert(entry); }
@@ -106,7 +139,7 @@ impl SqliteDb {
     let conn = rusqlite::Connection::open_in_memory().unwrap();
     conn.execute_batch(include_str!("schema.sql"))
       .unwrap();
-    
+
     SqliteDb { conn: conn }
   }
 }
@@ -116,41 +149,44 @@ impl Db for SqliteDb {
     unimplemented!()
   }
 
-  fn all_datoms<'a>(&'a self) -> Datoms {
-    let mut stmt = self.conn.prepare("select e,a,v,t,retracted from datoms;").unwrap();
-    let iter = stmt.query_map(&[], |row| {
-      // let value = unimplemented!();
-      Datom {
-        entity: EntityId(row.get(0)),
-        attribute: Attribute(EntityId(row.get(1))),
-        value: Value::Str("".into()),
-        tx: TxId(row.get(3)),
-        status: match row.get(4) {
-          0 => Status::Added,
-          1 => Status::Retracted,
-          unknown => panic!("Invalid status: {:?}", unknown)
-        }
-      }
-    }).unwrap();
-      
-    let mut datoms = Vec::new();
-    for d in iter {
-      datoms.push(d.unwrap());
-    }
-    datoms.into()
+  fn datoms<'a, C: Borrow<Components>>(&'a self, _index: Index, _components: C) -> Datoms {
+    unimplemented!()
   }
+
+  // fn all_datoms<'a>(&'a self) -> Datoms {
+  //   let mut stmt = self.conn.prepare("select e,a,v,t,retracted from datoms order by t asc").unwrap();
+  //   let iter = stmt.query_map(&[], |row| {
+  //     Datom {
+  //       entity: EntityId(row.get(0)),
+  //       attribute: Attribute(EntityId(row.get(1))),
+  //       value: row.get(2),
+  //       tx: TxId(row.get(3)),
+  //       status: match row.get(4) {
+  //         0 => Status::Added,
+  //         1 => Status::Retracted,
+  //         unknown => panic!("Invalid status: {:?}", unknown)
+  //       }
+  //     }
+  //   }).unwrap();
+
+  //   let mut datoms = Vec::new();
+  //   for d in iter {
+  //     datoms.push(d.unwrap());
+  //   }
+  //   datoms.into()
+  // }
 
   fn store_datoms(&mut self, datoms: &[Datom]) {
     let mut stmt = self.conn.prepare("insert into datoms (e,a,v,t,retracted) values (?1, ?2, ?3, ?4, ?5)")
       .unwrap();
     for d in datoms {
       let retracted = match d.status {
-        Status::Added => 1,
-        Status::Retracted => 0,
+        Status::Added => 0,
+        Status::Retracted => 1,
       };
       stmt.execute(&[&(d.entity.0),
                      &(d.attribute.0).0,
-                     &"",
+                     &d.value,
                      &d.tx.0,
                      &retracted])
         .unwrap();
@@ -158,14 +194,61 @@ impl Db for SqliteDb {
   }
 }
 
+impl rusqlite::types::FromSql for Value {
+  fn column_result(value: rusqlite::types::ValueRef) -> rusqlite::types::FromSqlResult<Self> {
+    use rusqlite::types::ValueRef;
+
+    match value {
+      ValueRef::Text(t)    => Ok(Value::Str(t.into())),
+      ValueRef::Integer(i) => Ok(Value::Int(i)),
+      _                    => unreachable!() // TODO
+    }
+  }
+}
+
+impl rusqlite::types::ToSql for Value {
+  fn to_sql(&self) -> rusqlite::Result<rusqlite::types::ToSqlOutput> {
+    use rusqlite::types::{ValueRef, ToSqlOutput};
+    match self {
+      &Value::Str(ref s) => Ok(ToSqlOutput::Borrowed(ValueRef::Text(s))),
+      &Value::Int(i) => Ok(ToSqlOutput::Owned(rusqlite::types::Value::Integer(i)))
+    }
+  }
+}
 
 #[cfg(test)]
 mod test_data;
 
 #[cfg(test)]
-mod test {
+mod tests {
   use super::*;
-  
+
+  #[test]
+  fn test_components() {
+    let d = Datom {
+      entity:    EntityId(42),
+      attribute: Attribute(EntityId(1)),
+      value:     Value::Int(23),
+      tx:        TxId(10),
+      status:    Status::Added,
+    };
+
+    assert_eq!(true, Components(None, None, None, None).matches(&d));
+
+    assert_eq!(true,  Components(Some(d.entity), None,              None,                  None).matches(&d));
+    assert_eq!(true,  Components(None,           Some(d.attribute), None,                  None).matches(&d));
+    assert_eq!(true,  Components(None,           None,              Some(d.value.clone()), None).matches(&d));
+    assert_eq!(true,  Components(None,           None,              None,                  Some(d.tx)).matches(&d));
+    assert_eq!(true,  Components(Some(d.entity), Some(d.attribute), None,                  None).matches(&d));
+    assert_eq!(true,  Components(Some(d.entity), Some(d.attribute), Some(d.value.clone()), None).matches(&d));
+    assert_eq!(true,  Components(Some(d.entity), Some(d.attribute), Some(d.value.clone()), Some(d.tx)).matches(&d));
+
+    assert_eq!(false, Components(Some(EntityId(999)), None, None, None).matches(&d));
+    assert_eq!(false, Components(None, Some(Attribute(EntityId(999))), None, None).matches(&d));
+    assert_eq!(false, Components(None, None, Some(Value::Int(1000)),None).matches(&d));
+    assert_eq!(false, Components(None, None, None, Some(TxId(999))).matches(&d));
+  }
+
   #[derive(Debug)]
   struct TestDb(Vec<Datom>);
 
@@ -178,28 +261,109 @@ mod test {
       self.0.clear();
       self.0.extend_from_slice(datoms);
     }
-    
-    fn all_datoms<'a>(&'a self) -> Datoms {
-      Cow::Borrowed(&self.0)
+
+    fn datoms<'a, C: Borrow<Components>>(&'a self, index: Index, components: C) -> Datoms {
+      let mut datoms = self.0.clone();
+      datoms.retain(|d| components.borrow().matches(&d));
+
+      datoms.sort_by(|l,r| {
+        use std::cmp::Ordering;
+        macro_rules! cmp {
+          ($i:ident) => (l.$i.cmp(&r.$i));
+          ($($i:ident),*) => {
+            [$(cmp!($i)),*].into_iter().fold(Ordering::Equal, |o, x| o.then(*x))
+
+          };
+        }
+
+        match index {
+          Index::Eavt => cmp!(tx, entity, attribute, value),
+        }
+      });
+
+      struct EavEquality(Datom);
+
+      impl PartialEq for EavEquality {
+        fn eq(&self, rhs: &EavEquality) -> bool {
+          let EavEquality(ref lhs) = *self;
+          let EavEquality(ref rhs) = *rhs;
+          lhs.entity == rhs.entity
+            && lhs.attribute == rhs.attribute
+            && lhs.value == rhs.value
+        }
+      }
+
+      use std::collections::BTreeMap;
+      use std::collections::btree_map::Entry;
+
+      let mut ds: BTreeMap<(EntityId,Attribute), (&Value, TxId, Status)> = BTreeMap::new();
+
+      for d in datoms.iter() {
+        let item = (d.entity, d.attribute);
+
+        match ds.entry(item) {
+          Entry::Vacant(_) if d.status == Status::Retracted => { }
+          Entry::Vacant(e)       => { e.insert((&d.value, d.tx, d.status)); }
+          Entry::Occupied(mut e) => {
+            let (value, tx, _) = *e.get();
+            if d.tx > tx {
+              match d.status {
+                // Newer datom was added, replace it
+                Status::Added => {
+                  e.insert((&d.value, d.tx, d.status));
+                },
+                // If retracted and the retracted value matches the
+                // datom's value, apply retraction
+                Status::Retracted if *value == d.value => {
+                  e.remove();
+                },
+                Status::Retracted => {}
+              }
+            }
+          }
+        }
+      }
+
+      let datoms = ds.into_iter()
+        .map(|((e,a),(v, tx, s))| {
+          Datom {
+            entity: e,
+            attribute: a,
+            value: v.clone(),
+            tx: tx,
+            status: s,
+          }
+        })
+        .collect();
+
+      Cow::Owned(datoms)
     }
   }
 
-  #[test]
-  fn test_default_impl() {
-    test_entity(TestDb(vec![]));
+  macro_rules! test_db_impl {
+    ($name:ident, $t:expr) => {
+      mod $name {
+        use super::*;
+        #[test]
+        fn test_entity() {
+          super::test_entity(($t));
+        }
+        #[test]
+        fn test_datoms() {
+          super::test_datoms(($t));
+        }
+      }
+    }
   }
 
-    #[test]
-  fn test_sqlite_impl() {
-    test_entity(SqliteDb::new());
-  }
-
+  test_db_impl!(in_memory, TestDb(vec![]));
+  test_db_impl!(sqlite,    SqliteDb::new());
 
   fn test_entity<D: Db>(mut db: D) {
     db.store_datoms(&test_data::make_test_data());
 
     assert_eq!(db.entity(EntityId(99999)).values.len(), 0);
-    
+
     let heinz = db.entity(EntityId(1)).values;
     assert_eq!(heinz.len(), 2);
     assert_eq!(heinz.get(&test_data::person_name), Some(&Value::Str("Heinz".into())));
@@ -212,5 +376,41 @@ mod test {
     let nevermind = db.entity(EntityId(3)).values;
     assert_eq!(nevermind.len(), 1);
     assert_eq!(nevermind.get(&test_data::album_name), Some(&Value::Str("Nevermind".into())));
+  }
+
+  fn test_datoms<D: Db>(mut db: D) {
+    db.store_datoms(&test_data::make_test_data());
+
+    let pn = test_data::person_name;
+    let pa = test_data::person_age;
+    let an = test_data::album_name;
+
+    let heinz     = EntityId(1);
+    let karl      = EntityId(2);
+    let nevermind = EntityId(3);
+
+    let eavt = db.datoms(Index::Eavt, Components::empty());
+    assert_eq!(eavt.iter().map(|d| d.entity).collect::<Vec<_>>(),
+               vec![heinz,heinz,karl,nevermind]);
+    assert_eq!(eavt.iter().map(|d| d.attribute).collect::<Vec<_>>(),
+               vec![pn, pa, pn, an]);
+
+    // None
+    let eavt = db.datoms(Index::Eavt, Components(Some(EntityId(99999)), None, None, None));
+    assert!(eavt.iter().map(|d| d.entity).collect::<Vec<_>>().is_empty());
+
+    // Heinz
+    let eavt = db.datoms(Index::Eavt, Components(Some(heinz), None, None, None));
+    assert_eq!(eavt.iter().map(|d| d.entity).collect::<Vec<_>>(),
+               vec![heinz,heinz]);
+    assert_eq!(eavt.iter().map(|d| d.attribute).collect::<Vec<_>>(),
+               vec![pn, pa]);
+
+    // Nevermind
+    let eavt = db.datoms(Index::Eavt, Components(Some(nevermind), None, None, None));
+    assert_eq!(eavt.iter().map(|d| d.entity).collect::<Vec<_>>(),
+               vec![nevermind]);
+    assert_eq!(eavt.iter().map(|d| d.attribute).collect::<Vec<_>>(),
+               vec![an]);
   }
 }
