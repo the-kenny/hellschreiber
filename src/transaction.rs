5e7e228b5fc282975d13d765a0c97ffcbada7eaa
Moritz Ulrich <moritz@tarn-vedra.de>
2018-05-21 03:25:26 +0200

    ToOperation: Use `ToAttribute` instead of `Attribute`

--- src/transaction.rs
+++ src/transaction.rs
@@ -14,24 +14,26 @@ pub trait ToOperation {
   fn to_operation<D: Db>(&self, db: &D) -> Operation;
 }
 
-impl<'a, V: Into<Value> + Clone, A: ToAttribute> ToOperation for &'a (Assert, TempId, A, V) {
+impl<'a, V, A> ToOperation for &'a (Assert, TempId, A, V)
+  where V: Into<Value> + Clone, A: ToAttribute {
   fn to_operation<D: Db>(&self, db: &D) -> Operation {
-    let a = self.2.to_attribute(db)
-      .expect("Unknown attribute in transaction");
+    let a = self.2.to_attribute(db).expect("Unknown attribute in transaction");
     Operation::TempidAssertion(self.1, a, self.3.clone().into())
   }
 }
 
-impl<'a, V> ToOperation for &'a (Assert, EntityId, Attribute, V)
-  where V: Into<Value> + Clone {
-  fn to_operation<D: Db>(&self, _db: &D) -> Operation {
-    Operation::Assertion(self.1, self.2, self.3.clone().into())
+impl<'a, V, A> ToOperation for &'a (Assert, EntityId, A, V)
+  where V: Into<Value> + Clone, A: ToAttribute {
+  fn to_operation<D: Db>(&self, db: &D) -> Operation {
+    let a = self.2.to_attribute(db).expect("Unknown attribute in transaction");
+    Operation::Assertion(self.1, a, self.3.clone().into())
   }
 }
 
-impl<'a, V> ToOperation for &'a (Retract, EntityId, Attribute, V)
-  where V: Into<Value> + Clone {
-  fn to_operation<D: Db>(&self, _db: &D) -> Operation {
-    Operation::Retraction(self.1, self.2, self.3.clone().into())
+impl<'a, V, A> ToOperation for &'a (Retract, EntityId, A, V)
+  where V: Into<Value> + Clone, A: ToAttribute {
+  fn to_operation<D: Db>(&self, db: &D) -> Operation {
+    let a = self.2.to_attribute(db).expect("Unknown attribute in transaction");
+    Operation::Retraction(self.1, a, self.3.clone().into())
   }
 }
